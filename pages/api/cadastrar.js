64a85c05c62599657ac8667cc63d854f5e6392de
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2024-09-02 12:22:36 -0300

    vuvgh

--- pages/api/cadastrar.js
+++ pages/api/cadastrar.js
@@ -4,5 +4,5 @@ export default async function cadastrar(request,response){
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE");
     response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    response.json(request);
+    response.json({nome:"pedro"});
 }
